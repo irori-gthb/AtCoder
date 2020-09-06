e3660bddc0ee81723b47b2b6a2d45c4e5a448c07
irori <cats.cafemocha+irori@gmail.com>
2020-09-07 00:15:50 +0900

    20200907_02

---
+++ abc175/abc175_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S == "RRR")
+    cout << 3 << endl;
+  else if (S == "SRR" || S == "RRS")
+    cout << 2 << endl;
+  else if (S == "SSR" || S == "RSS" || S == "SRS" || S == "RSR")
+    cout << 1 << endl;
+  else if (S == "SSS")
+    cout << 0 << endl;
+
+  return 0;
+}
