26dfa440efb5fcbdab4f9577d7fdd9ac4107fd3b
irori <cats.cafemocha+irori@gmail.com>
2020-09-09 01:06:31 +0900

    20200909_08

---
+++ abc155/abc155_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  if ((A == B && B != C) || (B == C && C != A) || (C == A && A != B))
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}
