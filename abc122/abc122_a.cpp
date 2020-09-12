01e39a77e2a215b0388807d0cc13d6add5020f03
irori <cats.cafemocha+irori@gmail.com>
2020-09-12 23:48:02 +0900

    20200912_15

---
+++ abc122/abc122_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  char b;
+  cin >> b;
+
+  if (b == 'A')
+    cout << 'T' << endl;
+  else if (b == 'T')
+    cout << 'A' << endl;
+  else if (b == 'C')
+    cout << 'G' << endl;
+  else if (b == 'G')
+    cout << 'C' << endl;
+  return 0;
+}
