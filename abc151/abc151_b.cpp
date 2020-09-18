5b59c25b674f56b861874151b0aa6b2807410396
irori <cats.cafemocha+irori@gmail.com>
2020-09-19 01:51:05 +0900

    20200919_18

---
+++ abc151/abc151_b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, K, M;
+  cin >> N >> K >> M;
+  int A;
+  int sum = 0;
+
+  rep(i, N - 1) {
+    cin >> A;
+    sum += A;
+  }
+
+  if (N * M > sum + K)
+    cout << -1 << endl;
+  else
+    cout << max(0, N * M - sum);
+
+  return 0;
+}
