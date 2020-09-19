5fd5f9952d5d3c2a2a8de5f71b5dbb3f950060ac
irori <cats.cafemocha+irori@gmail.com>
2020-09-19 23:50:24 +0900

    20200919_53

---
+++ codefestival_2016_qualC/codefestival_2016_qualC_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string s;
+  cin >> s;
+
+  int c = -1;
+
+  rep(i, s.size()) {
+    if (c == -1 && s[i] == 'C') {
+      c = i;
+      continue;
+    }
+
+    if (c != -1 && s[i] == 'F') {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  cout << "No" << endl;
+  return 0;
+}
