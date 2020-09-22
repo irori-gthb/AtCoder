92097675bc09c069800de275ea499187f9c4219c
irori <cats.cafemocha+irori@gmail.com>
2020-09-22 22:22:38 +0900

    20200922_31

---
+++ abc021/abc021_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << N << endl;
+  rep(i, N) cout << 1 << endl;
+  return 0;
+}
