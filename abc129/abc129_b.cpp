f52f023f7e7bc995f879bf3a47702410eeeb8cce
irori <cats.cafemocha+irori@gmail.com>
2020-09-24 00:18:39 +0900

    20200924_01

---
+++ abc129/abc129_b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> W(N);
+  int sum = 0;
+
+  rep(i, N) {
+    cin >> W[i];
+    sum += W[i];
+  }
+
+  int ans = INT_MAX;
+  int S1 = 0;
+  rep(i, N) {
+    S1 += W[i];
+    ans = min(ans, abs(S1 - (sum - S1)));
+  }
+
+  cout << ans << endl;
+  return 0;
+}
