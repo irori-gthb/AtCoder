a4301abd2e857f5a6289f0bea7a12cf13e982254
irori <cats.cafemocha+irori@gmail.com>
2020-09-23 20:41:54 +0900

    20200923_23

---
+++ abc162/abc162_c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+int main() {
+  int K;
+  cin >> K;
+
+  int ans = 0;
+  for (int x = 1; x <= K; x++) {
+    for (int y = 1; y <= K; y++) {
+      for (int z = 1; z <= K; z++) {
+        ans += gcd(x, gcd(y, z));
+      }
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
