1864d4188a8e83b3cb274c1a2a4eea8e9015db62
irori <cats.cafemocha+irori@gmail.com>
2020-09-23 12:16:28 +0900

    20200923_04

---
+++ abc017/abc017_a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  vector<int> s(3);
+  vector<int> e(3);
+  rep(i, 3) {
+    cin >> s[i];
+    cin >> e[i];
+  }
+
+  int ans = 0;
+  rep(i, 3) ans += s[i] * e[i];
+  cout << ans / 10 << endl;
+  return 0;
+}
