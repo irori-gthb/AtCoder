f9cae740771ba1a8fdff8be682cab46f7d068d72
irori <cats.cafemocha+irori@gmail.com>
2020-09-23 12:15:58 +0900

    20200923_03

---
+++ abc018/abc018_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  cout << 1 + (A < B) + (A < C) << endl;
+  cout << 1 + (B < A) + (B < C) << endl;
+  cout << 1 + (C < B) + (C < A) << endl;
+  return 0;
+}
