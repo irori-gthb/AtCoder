84f9eea358edfee22424fa7a1e4ee148166c70a2
irori <cats.cafemocha+irori@gmail.com>
2020-09-23 09:35:24 +0900

    20200923_01

---
+++ abc020/abc020_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int Q;
+  cin >> Q;
+
+  if (Q == 1)
+    cout << "ABC" << endl;
+  else
+    cout << "chokudai" << endl;
+  return 0;
+}
