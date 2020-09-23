440f6723b55ae3132247ce9ca9d38a77c12ee54c
irori <cats.cafemocha+irori@gmail.com>
2020-09-23 20:39:14 +0900

    20200923_20

---
+++ abc001/abc001_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int H1, H2;
+  cin >> H1 >> H2;
+
+  cout << H1 - H2 << endl;
+  return 0;
+}
