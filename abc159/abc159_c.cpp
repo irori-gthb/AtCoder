8e9a306ea756040f462c8f8519559c0c597df474
irori <cats.cafemocha+irori@gmail.com>
2020-09-24 23:21:55 +0900

    20200924_14

---
+++ abc159/abc159_c.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double L;
+  cin >> L;
+
+  cout << setprecision(10) << pow(L / 3, 3) << endl;
+  return 0;
+}
