0674c45775990ee1228869d7098d87a4cd6c14c5
irori <cats.cafemocha+irori@gmail.com>
2020-09-24 12:18:17 +0900

    20200924_04

---
+++ abc121/abc121_b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M, C;
+  cin >> N >> M >> C;
+  vector<int> B(M);
+  vector<vector<int>> A(N, vector<int>(M));
+
+  rep(i, M) cin >> B[i];
+  rep(i, N) rep(j, M) cin >> A[i][j];
+
+  int ans = 0;
+  int calc = 0;
+  rep(i, N) {
+    rep(j, M) calc += A[i][j] * B[j];
+    if (calc + C > 0) ans++;
+    calc = 0;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
