f8037743437bb7756375ac0e76a2b3d04ebf1a23
irori <cats.cafemocha+irori@gmail.com>
2020-09-25 20:12:45 +0900

    20200925_09

---
+++ arc003/arc003_a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  map<char, int> mp;
+  char r;
+  rep(i, N) {
+    cin >> r;
+    mp[r]++;
+  }
+
+  map<char, int> eva;
+  eva['A'] = 4;
+  eva['B'] = 3;
+  eva['C'] = 2;
+  eva['D'] = 1;
+  eva['F'] = 0;
+  double calc = 0;
+  for (auto x : mp) calc += eva[x.first] * x.second;
+
+  cout << setprecision(10) << calc / N << endl;
+  return 0;
+}
