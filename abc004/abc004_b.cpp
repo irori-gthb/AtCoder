4cd666c69214a2d8227b4f8023dc679ad1c3a27d
irori <cats.cafemocha+irori@gmail.com>
2020-09-26 00:52:19 +0900

    20200926_08

---
+++ abc004/abc004_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  vector<vector<char>> c(4, vector<char>(4));
+  rep(i, 4) rep(j, 4) cin >> c[i][j];
+
+  rep(i, 4) {
+    rep(j, 4) cout << c[3 - i][3 - j] << " ";
+    cout << endl;
+  }
+  return 0;
+}
