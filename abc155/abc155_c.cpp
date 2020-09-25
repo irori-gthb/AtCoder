b664252b03301acb27f8cd68c6ca648e0b31b1e8
irori <cats.cafemocha+irori@gmail.com>
2020-09-25 12:06:35 +0900

    20200925_06

---
+++ abc155/abc155_c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  map<string, int> mp;
+  string S;
+  rep(i, N) {
+    cin >> S;
+    mp[S]++;
+  }
+
+  int ms = 0;
+  for (auto x : mp) ms = max(ms, x.second);
+
+  vector<string> ans;
+  for (auto x : mp) {
+    if (ms == x.second) ans.push_back(x.first);
+  }
+
+  sort(ans.begin(), ans.end());
+  rep(i, ans.size()) cout << ans[i] << endl;
+  return 0;
+}
