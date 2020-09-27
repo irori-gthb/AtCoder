eb5b6a5062e6ae22dab7d9afc0eaa9ca3f2c7f58
irori <cats.cafemocha+irori@gmail.com>
2020-09-27 20:42:05 +0900

    20200927_08

---
+++ abc012/abc012_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << setfill('0') << right << setw(2) << N / 3600;
+  cout << ":";
+  cout << setfill('0') << right << setw(2) << (N % 3600) / 60;
+  cout << ":";
+  cout << setfill('0') << right << setw(2) << N % 60;
+  return 0;
+}
