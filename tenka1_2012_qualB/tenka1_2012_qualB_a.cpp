c47162fae440ff57e56d0a150ccdc7963e0268f3
irori <cats.cafemocha+irori@gmail.com>
2020-09-27 11:58:43 +0900

    20200927_04

---
+++ tenka1_2012_qualB/tenka1_2012_qualB_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  for (int i = 1; i <= 127; i++)
+    if (i % 3 == a && i % 5 == b && i % 7 == c) cout << i << endl;
+  return 0;
+}
