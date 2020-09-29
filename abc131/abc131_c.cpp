c18009d2da1b5c9c2d01f621acd5e908f054ef22
irori <cats.cafemocha+irori@gmail.com>
2020-09-29 21:20:14 +0900

    20200929_04

---
+++ abc131/abc131_c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int gcd(ll a, ll b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+int main() {
+  ll A, B, C, D;
+  cin >> A >> B >> C >> D;
+
+  ll mul_C = B / C - (A - 1) / C;
+  ll mul_D = B / D - (A - 1) / D;
+  ll cd = C * D / gcd(C, D);
+  ll mul_CD = B / cd - (A - 1) / cd;
+  cout << B - A - mul_C + 1 - mul_D + mul_CD << endl;
+  return 0;
+}
