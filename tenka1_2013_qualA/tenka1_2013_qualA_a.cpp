0a4e68840db28daa075f37e003105672e215419a
irori <cats.cafemocha+irori@gmail.com>
2020-09-30 09:15:49 +0900

    20200930_12

---
+++ tenka1_2013_qualA/tenka1_2013_qualA_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int ans = 42;
+  while (ans < 130000000) ans *= 2;
+  cout << ans << endl;
+  return 0;
+}
