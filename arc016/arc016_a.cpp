cbcd64d564d570d1af93739817323ebfc52b6479
irori <cats.cafemocha+irori@gmail.com>
2020-10-01 23:10:25 +0900

    20201001_15

---
+++ arc016/arc016_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  cout << (M == 1 ? 2 : 1) << endl;
+  return 0;
+}
