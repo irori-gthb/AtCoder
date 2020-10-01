1e66069b674e2029764b63bfa592033af6b4ab11
irori <cats.cafemocha+irori@gmail.com>
2020-10-02 00:42:25 +0900

    20201001_02

---
+++ abc039/abc039_c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+  S = S.substr(0, 12);
+  if (S == "WBWBWWBWBWBW")
+    cout << "Do" << endl;
+  else if (S == "WBWWBWBWBWWB")
+    cout << "Re" << endl;
+  else if (S == "WWBWBWBWWBWB")
+    cout << "Mi" << endl;
+  else if (S == "WBWBWBWWBWBW")
+    cout << "Fa" << endl;
+  else if (S == "WBWBWWBWBWWB")
+    cout << "So" << endl;
+  else if (S == "WBWWBWBWWBWB")
+    cout << "La" << endl;
+  else if (S == "WWBWBWWBWBWB")
+    cout << "Si" << endl;
+  return 0;
+}
