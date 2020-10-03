d91a1efec035b0db32e480fd8acce5e53d3122de
irori <cats.cafemocha+irori@gmail.com>
2020-10-03 19:29:49 +0900

    20201003_06

---
+++ abc007/abc007_b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string A;
+  cin >> A;
+  if (A == "a")
+    cout << -1 << endl;
+  else
+    cout << "a" << endl;
+  return 0;
+}
