45e29036bac0adfb3458ca6ddbc3247ced64b861
irori <cats.cafemocha+irori@gmail.com>
2020-10-05 12:20:16 +0900

    20201005_08

---
+++ arc024/arc024_a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int L, R;
+  cin >> L >> R;
+
+  int l, r;
+  map<int, int> lm;
+  map<int, int> rm;
+  rep(i, L) {
+    cin >> l;
+    lm[l]++;
+  }
+  rep(i, R) {
+    cin >> r;
+    rm[r]++;
+  }
+
+  int ans = 0;
+  for (auto x : rm) {
+    ans += min(x.second, lm[x.first]);
+  }
+  cout << ans << endl;
+  return 0;
+}
