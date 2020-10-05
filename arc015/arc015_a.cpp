560197ff7167a5a502033446cfe27cabf96aa26a
irori <cats.cafemocha+irori@gmail.com>
2020-10-05 12:19:02 +0900

    20201005_06

---
+++ arc015/arc015_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double n;
+  cin >> n;
+
+  cout << setprecision(10) << (9.0 / 5.0 * n) + 32 << endl;
+  return 0;
+}
