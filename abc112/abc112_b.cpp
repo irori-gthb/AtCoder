ccaffe3ad2c223794449e14119420e85305afc5a
irori <cats.cafemocha+irori@gmail.com>
2020-10-06 00:04:22 +0900

    20201006_01

---
+++ abc112/abc112_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, T;
+  cin >> N >> T;
+
+  int c, t;
+  int ans = 1001;
+  rep(i, N) {
+    cin >> c >> t;
+    if (t <= T) ans = min(ans, c);
+  }
+  if (ans == 1001) {
+    cout << "TLE" << endl;
+    return 0;
+  }
+  cout << ans << endl;
+  return 0;
+}
