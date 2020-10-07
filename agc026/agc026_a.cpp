4a529f9433d600a58f1ff50de6e04844a090d367
irori <cats.cafemocha+irori@gmail.com>
2020-10-07 12:32:57 +0900

    20201007_04

---
+++ agc026/agc026_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> a(N);
+  rep(i, N) cin >> a[i];
+
+  int ans = 0;
+  int cnt = 1;
+  for (int i = 1; i < N; i++) {
+    if (a[i - 1] == a[i])
+      cnt++;
+    else {
+      ans += cnt / 2;
+      cnt = 1;
+    }
+  }
+  ans += cnt / 2;
+  cout << ans << endl;
+  return 0;
+}
