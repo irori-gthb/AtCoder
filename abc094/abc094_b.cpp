edfdf199ae8041f555a563843d0388cd43edb072
irori <cats.cafemocha+irori@gmail.com>
2020-10-08 22:27:55 +0900

    20201008_03

---
+++ abc094/abc094_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M, X;
+  cin >> N >> M >> X;
+
+  int toN = 0;
+  int to0 = 0;
+  int A;
+  rep(i, M) {
+    cin >> A;
+    if (A > X)
+      toN++;
+    else
+      to0++;
+  }
+  cout << min(toN, to0) << endl;
+  return 0;
+}
