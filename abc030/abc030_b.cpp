f250b8d771dad8acc53d85a60346e52a51c8424e
irori <cats.cafemocha+irori@gmail.com>
2020-10-08 22:39:49 +0900

    20201008_05

---
+++ abc030/abc030_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n;
+  double m;
+  cin >> n >> m;
+
+  double hs = 30 * (n % 12) + m / 2;
+  double ms = m * 6;
+  cout << min(abs(hs - ms), 360 - abs(hs - ms)) << endl;
+  return 0;
+}
