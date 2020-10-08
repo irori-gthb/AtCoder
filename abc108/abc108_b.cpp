ec6af8c9f6a19e5bba5e4a8d0db95ecebdd5a131
irori <cats.cafemocha+irori@gmail.com>
2020-10-08 22:18:26 +0900

    20201008_02

---
+++ abc108/abc108_b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+
+  cout << x2 + y1 - y2 << " " << y2 + x2 - x1 << " " << x1 + y1 - y2 << " "
+       << y1 + x2 - x1 << endl;
+  return 0;
+}
