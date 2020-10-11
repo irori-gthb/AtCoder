68f26680f11e3a16990504d0b5fe856b35346031
irori <cats.cafemocha+irori@gmail.com>
2020-10-11 14:08:56 +0900

    20201011_03

---
+++ yahoo_procon2019_qual/yahoo_procon2019_qual_b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int x;
+  map<int, int> mp;
+
+  rep(i, 6) {
+    cin >> x;
+    mp[x]++;
+  }
+
+  for (auto x : mp) {
+    if (x.second >= 3) {
+      cout << "NO" << endl;
+      return 0;
+    }
+  }
+  cout << "YES" << endl;
+  return 0;
+}
