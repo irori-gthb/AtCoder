2d27429047ce78338ca02e22bd0b3e05138a450c
irori <cats.cafemocha+irori@gmail.com>
2020-10-15 09:46:53 +0900

    20201015_03

---
+++ arc028/arc028_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, A, B;
+  cin >> N >> A >> B;
+
+  int calc = (N - 1) % (A + B);
+  if (calc < A)
+    cout << "Ant" << endl;
+  else
+    cout << "Bug" << endl;
+  return 0;
+}
