80b27baae87acc44ea496b083663ee15ed4c3353
irori <cats.cafemocha+irori@gmail.com>
2020-10-16 18:52:14 +0900

    20201016_01

---
+++ abc081/abc081_b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  rep(i, N) cin >> A[i];
+
+  int calc = A[0];
+  rep(i, N) calc = gcd(calc, A[i]);
+
+  int ans = 0;
+  while (calc % 2 == 0) {
+    calc /= 2;
+    ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
