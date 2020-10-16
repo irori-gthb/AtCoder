4069dd6d3059871877ae0e61b990316618d803f7
irori <cats.cafemocha+irori@gmail.com>
2020-10-17 01:20:30 +0900

    20201017_01

---
+++ abc118/abc118_b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  int A, K;
+  map<int, int> mp;
+  rep(i, N) {
+    cin >> K;
+    rep(j, K) {
+      cin >> A;
+      mp[A]++;
+    }
+  }
+
+  int ans = 0;
+  for (auto x : mp) {
+    if (x.second == N) ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
