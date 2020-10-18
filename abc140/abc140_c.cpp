5cd45d83bd7f8ddaf146e0a1b171db4c98ac8c52
irori <cats.cafemocha+irori@gmail.com>
2020-10-19 01:17:37 +0900

    20201019_03

---
+++ abc140/abc140_c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> B(N);
+  rep(i, N - 1) cin >> B[i];
+
+  int ans = 0;
+  for (int i = 1; i < N - 1; i++) {
+    ans = ans + min(B[i], B[i - 1]);
+  }
+  ans = ans + B[0] + B[N - 2];
+  cout << ans << endl;
+  return 0;
+}
