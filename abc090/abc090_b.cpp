08d1283fe249eefad373a56c258b0d22771ff6be
irori <cats.cafemocha+irori@gmail.com>
2020-10-18 19:32:14 +0900

    20201018_04

---
+++ abc090/abc090_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int ans = 0;
+  for (int i = A; i <= B; i++) {
+    if (i / 10000 == i % 10 && (i % 10000) / 1000 == (i % 100) / 10) ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
