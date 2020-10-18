2aa68306bc47c350401500120ed6373ed30d2d9a
irori <cats.cafemocha+irori@gmail.com>
2020-10-19 00:51:55 +0900

    20201019_02

---
+++ abc061/abc061_b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  map<int, int> mp;
+  int a;
+  rep(i, 2 * M) {
+    cin >> a;
+    mp[a]++;
+  }
+
+  rep(i, N) cout << mp[i + 1] << endl;
+  return 0;
+}
