cb710566a0c9e3c9dc26b22f1cbcd30ad55d598b
irori <cats.cafemocha+irori@gmail.com>
2020-10-20 00:49:59 +0900

    20201020_02

---
+++ arc022/arc022_a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  int i = -1;
+  int c = -1;
+  rep(x, S.size()) {
+    if (i == -1 && (char)tolower(S[x]) == 'i') i = x;
+    if (i != -1 && c == -1 && (char)tolower(S[x]) == 'c') c = x;
+    if (c != -1 && (char)tolower(S[x]) == 't') {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+  cout << "NO" << endl;
+  return 0;
+}
