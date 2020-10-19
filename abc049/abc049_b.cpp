2aba3c32a94638b17720c0f11b10f3ff78752636
irori <cats.cafemocha+irori@gmail.com>
2020-10-19 22:57:07 +0900

    20201019_05

---
+++ abc049/abc049_b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int H, W;
+  cin >> H >> W;
+
+  vector<string> C(H);
+  rep(i, H) cin >> C[i];
+
+  rep(i, H) {
+    cout << C[i] << endl;
+    cout << C[i] << endl;
+  }
+  return 0;
+}
