37eed02bab26fe187916435c06cd7ec0a1194a5f
irori <cats.cafemocha+irori@gmail.com>
2020-10-25 10:22:59 +0900

    20201025_02

---
+++ abc036/abc036_b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<string> s(N);
+  rep(i, N) cin >> s[i];
+
+  rep(i, N) {
+    rep(j, N) cout << s[N - j - 1][i];
+    cout << endl;
+  }
+  return 0;
+}
