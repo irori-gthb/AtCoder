2280adb3675ae7db3c804efaf3c464768a439a03
irori <cats.cafemocha+irori@gmail.com>
2020-10-26 23:12:25 +0900

    20201026_04

---
+++ abc103/abc103_b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S, T;
+  cin >> S >> T;
+
+  string temp = S + S;
+  rep(i, S.size()) {
+    if (temp.substr(i, S.size()) == T) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
