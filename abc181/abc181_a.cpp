0462df49d11e3468b49d229e296eff4607c9ed11
irori <cats.cafemocha+irori@gmail.com>
2020-11-02 22:03:10 +0900

    20201102_01

---
+++ abc181/abc181_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  if (N % 2 == 0)
+    cout << "White" << endl;
+  else
+    cout << "Black" << endl;
+  return 0;
+}
