b8cb67dedf4ddc67c1eece0dd4186e21d6c29336
irori <cats.cafemocha+irori@gmail.com>
2020-11-07 13:20:36 +0900

    20201107_01

---
+++ abc053/abc053_b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string s;
+  cin >> s;
+
+  int a = 0;
+  int z = 0;
+  rep(i, s.size()) {
+    if (s[i] == 'A') {
+      a = i;
+      break;
+    }
+  }
+
+  rep(i, s.size()) {
+    if (s[s.size() - i - 1] == 'Z') {
+      z = s.size() - i - 1;
+      break;
+    }
+  }
+
+  cout << z - a + 1 << endl;
+  return 0;
+}
