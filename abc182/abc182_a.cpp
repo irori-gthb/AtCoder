bbd2bb3596afb6d68f9b871e9a8d006994d71713
irori <cats.cafemocha+irori@gmail.com>
2020-11-09 00:34:56 +0900

    20201109_02

---
+++ abc182/abc182_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  cout << 2 * A + 100 - B << endl;
+  return 0;
+}
