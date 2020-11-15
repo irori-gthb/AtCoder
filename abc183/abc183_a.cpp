fc1d1e2afed6a68066348b7310d79b07b06ac6c2
irori <cats.cafemocha+irori@gmail.com>
2020-11-16 00:38:03 +0900

    20201116_01

---
+++ abc183/abc183_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int x;
+  cin >> x;
+
+  if (x < 0)
+    cout << 0 << endl;
+  else
+    cout << x << endl;
+  return 0;
+}
