d4885f4bf2fbf993f1cbe80cc5ef692d8921acb8
irori <cats.cafemocha+irori@gmail.com>
2020-11-18 00:23:40 +0900

    20201118_01

---
+++ arc009/arc009_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int a, b;
+  int ans = 0;
+  rep(i, N) {
+    cin >> a >> b;
+    ans += a * b;
+  }
+
+  cout << (int)(ans * 1.05) << endl;
+  return 0;
+}
