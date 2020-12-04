3bd265d7beb10f3cbd413e3b9d17db87b8f180cd
irori <cats.cafemocha+irori@gmail.com>
2020-12-05 00:25:38 +0900

    20201205_01

---
+++ abc121/abc121_c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  vector<pair<ll, ll>> vec(N);
+  rep(i, N) cin >> vec[i].first >> vec[i].second;
+  sort(vec.begin(), vec.end());
+
+  ll ans = 0;
+  rep(i, N) {
+    if (vec[i].second >= M) {
+      cout << ans + M * vec[i].first << endl;
+      return 0;
+    } else {
+      M -= vec[i].second;
+      ans += vec[i].first * vec[i].second;
+    }
+  }
+}
