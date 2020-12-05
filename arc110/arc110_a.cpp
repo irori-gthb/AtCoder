8a742dd7d7ba350d5a3cf967ced6e38fc0fdcd68
irori <cats.cafemocha+irori@gmail.com>
2020-12-06 00:56:55 +0900

    20201206_01

---
+++ arc110/arc110_a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  ll ans = 1;
+  for (int i = 1; i <= N; i++) {
+    ll calc = gcd(ans, i);
+    ans = ans * i / calc;
+  }
+  cout << ans + 1 << endl;
+  return 0;
+}
