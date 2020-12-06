6267daa7f5093ab6770e1cad4a012bf78e0b5c78
irori <cats.cafemocha+irori@gmail.com>
2020-12-06 21:34:55 +0900

    20201206_06

---
+++ keyence2019/keyence2019_b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S == "keyence") {
+    cout << "YES" << endl;
+    return 0;
+  }
+
+  rep(i, 8) {
+    if (S.substr(0, i) + S.substr(S.size() + i - 7, 7 - i) == "keyence") {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+  cout << "NO" << endl;
+  return 0;
+}
