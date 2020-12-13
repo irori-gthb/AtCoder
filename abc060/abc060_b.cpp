73f494d106892c3183d567a1c52dd59fe3fd4544
irori <cats.cafemocha+irori@gmail.com>
2020-12-14 00:42:54 +0900

    20201214_01

---
+++ abc060/abc060_b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  for (int i = 1; i <= B; i++) {
+    if (i * A % B == C) {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+  cout << "NO" << endl;
+  return 0;
+}
