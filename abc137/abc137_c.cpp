703eeeaf1f79aabe947a2592605f942d2eeaf648
irori <cats.cafemocha+irori@gmail.com>
2020-12-19 09:37:32 +0900

    20201219_01

---
+++ abc137/abc137_c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  map<string, ll> mp;
+  string S;
+  rep(i, N) {
+    cin >> S;
+    sort(S.begin(), S.end());
+    mp[S]++;
+  }
+
+  ll ans = 0;
+  for (auto x : mp)
+    if (x.second >= 2) ans += x.second * (x.second - 1) / 2;
+  cout << ans << endl;
+  return 0;
+}
