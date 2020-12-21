fbb42ec8b3fbb25f9468452b57dc5f2ebb554bcf
irori <cats.cafemocha+irori@gmail.com>
2020-12-22 00:28:12 +0900

    20201222_01

---
+++ abc130/abc130_c.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  long double W, H, x, y;
+  cin >> W >> H >> x >> y;
+
+  cout << setprecision(10) << (W * H) / 2 << " ";
+  if (W / 2 == x && H / 2 == y)
+    cout << 1 << endl;
+  else
+    cout << 0 << endl;
+  return 0;
+}
