46c624c09462fa189a690c5b7deaa42b590f2330
irori <cats.cafemocha+irori@gmail.com>
2020-12-26 01:49:34 +0900

    20201226_01

---
+++ abc029/abc029_c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+void dfs(int n, string s) {
+  if (n == 0)
+    cout << s << endl;
+  else
+    for (char c = 'a'; c <= 'c'; c++) dfs(n - 1, s + c);
+}
+
+int main() {
+  int N;
+  cin >> N;
+  dfs(N, "");
+  return 0;
+}
