e4c975278a99bc87a424b339083729868183ddfb
irori <cats.cafemocha+irori@gmail.com>
2021-01-10 00:59:56 +0900

    20210110_01

---
+++ abc126/abc126_c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+
+  double ans = 0;
+  rep(i, N) {
+    int calc = i + 1;
+    double p = (double)1 / N;
+    while (calc < K) {
+      calc *= 2;
+      p /= 2;
+    }
+    ans += p;
+  }
+  cout << fixed << setprecision(10) << ans << endl;
+  return 0;
+}
