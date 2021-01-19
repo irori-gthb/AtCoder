22adc7da0aa2ac0c5a1fcbc9fa6e2aa227f95928
irori <cats.cafemocha+irori@gmail.com>
2021-01-20 00:33:02 +0900

    20210120_02

---
+++ code_festival_final/code_festival_final_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double s;
+  cin >> s;
+
+  cout << fixed << setprecision(5) << 50 / s << endl;
+  return 0;
+}
