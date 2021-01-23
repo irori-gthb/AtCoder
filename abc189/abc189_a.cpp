9c1d3bb32a516b33357c6fee2b0008d7ef4a493f
irori <cats.cafemocha+irori@gmail.com>
2021-01-24 00:51:41 +0900

    20210124_01

---
+++ abc189/abc189_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  char C1, C2, C3;
+  cin >> C1 >> C2 >> C3;
+
+  if (C1 == C2 && C2 == C3)
+    cout << "Won" << endl;
+  else
+    cout << "Lost" << endl;
+  return 0;
+}
