999e727526c97ce49d00e6c84972c2d2d0099de6
irori <cats.cafemocha+irori@gmail.com>
2021-02-08 00:39:49 +0900

    20210208_01

---
+++ abc023/abc023_b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  if (N % 2 == 0) {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  array<char, 3> arr = {'a', 'b', 'c'};
+  rep(i, N) {
+    if (S[i] != arr[(i + N) % 3]) {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+  cout << (N - 1) / 2 << endl;
+  return 0;
+}
