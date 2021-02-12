b91d79098cc7c24efd0af631cc3254c907e19cab
irori <cats.cafemocha+irori@gmail.com>
2021-02-12 12:16:44 +0900

    20210212_01

---
+++ abc0xx/abc038/abc038_c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  rep(i, N) cin >> a[i];
+
+  ll ans = N;
+  ll cnt = 1;
+  rep(i, N - 1) {
+    if (a[i] < a[i + 1])
+      cnt++;
+    else {
+      ans += cnt * (cnt - 1) / 2;
+      cnt = 1;
+    }
+  }
+  ans += cnt * (cnt - 1) / 2;
+  cout << ans << endl;
+  return 0;
+}
