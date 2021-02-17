8c98e9f10c8d07e01b8e7eae53afb71a7675721e
irori <cats.cafemocha+irori@gmail.com>
2021-02-17 21:23:08 +0900

    20210217_01

---
+++ abc0xx/abc051/abc051_b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int K, S;
+  cin >> K >> S;
+
+  ll ans = 0;
+  rep(i, K + 1) {
+    rep(j, K + 1) {
+      if (0 <= S - i - j && S - i - j <= K) ans++;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
