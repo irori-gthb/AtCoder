f57225f2b8ed48086709e004f27e7747fb376d7b
irori <cats.cafemocha+irori@gmail.com>
2021-02-21 00:28:58 +0900

    20210221_01

---
+++ abc1xx/abc192/abc192_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int X;
+  cin >> X;
+
+  cout << 100 - X % 100 << endl;
+  return 0;
+}
