f2322507f40bdc2551b9323c798c36b2eb1dec27
irori <cats.cafemocha+irori@gmail.com>
2021-02-23 01:45:40 +0900

    20210223_01

---
+++ abc1xx/abc192/abc192_c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int f(int N) {
+  string str = to_string(N);
+
+  sort(str.begin(), str.end(), greater<char>());
+  int g1 = stoi(str);
+
+  sort(str.begin(), str.end());
+  int g2 = stoi(str);
+
+  return g1 - g2;
+}
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+
+  rep(i, K) N = f(N);
+
+  cout << N << endl;
+  return 0;
+}
