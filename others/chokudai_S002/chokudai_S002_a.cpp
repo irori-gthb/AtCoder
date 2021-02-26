7b5e4ba52fd11ec0304c4760a1826ffeb6a698cb
irori <cats.cafemocha+irori@gmail.com>
2021-02-26 18:23:17 +0900

    20210226_01

---
+++ others/chokudai_S002/chokudai_S002_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<ll> ans(N);
+  rep(i, N) {
+    ll A, B;
+    cin >> A >> B;
+    ans[i] = A * B;
+  }
+
+  rep(i, N) cout << ans[i] << endl;
+  return 0;
+}
