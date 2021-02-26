00df43d8875d7c3f0384dd1fce9d45899f15f845
irori <cats.cafemocha+irori@gmail.com>
2021-02-26 18:46:49 +0900

    20210226_04

---
+++ others/chokudai_S002/chokudai_S002_d.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  ll ans = 0;
+  rep(i, N) {
+    ll A, B;
+    cin >> A >> B;
+    ans += max(A, B);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
