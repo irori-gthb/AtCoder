78d3670215bf2773587bcf057282398882437ae2
irori <cats.cafemocha+irori@gmail.com>
2021-02-28 00:07:29 +0900

    20210228_01

---
+++ abc1xx/abc193/abc193_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double A, B;
+  cin >> A >> B;
+
+  cout << fixed << setprecision(10) << 100 - B / A * 100 << endl;
+  return 0;
+}
