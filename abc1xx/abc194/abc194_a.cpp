4d80876605826e44857e484b4f5e10d31de6a293
irori <cats.cafemocha+irori@gmail.com>
2021-03-07 00:30:32 +0900

    20210307_01

---
+++ abc1xx/abc194/abc194_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  if (A + B >= 15 && B >= 8)
+    cout << 1 << endl;
+  else if (A + B >= 10 && B >= 3)
+    cout << 2 << endl;
+  else if (A + B >= 3)
+    cout << 3 << endl;
+  else
+    cout << 4 << endl;
+  return 0;
+}
