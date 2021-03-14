27e802e0a86f35483910ec5e675a6c0b5400b2dd
irori <cats.cafemocha+irori@gmail.com>
2021-03-15 00:08:52 +0900

    20210315_01

---
+++ abc1xx/abc195/abc195_c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll N;
+  cin >> N;
+
+  ll ans = 0;
+  if (N >= 1000) ans += N - 999;
+  if (N >= 1000000) ans += N - 999999;
+  if (N >= 1000000000) ans += N - 999999999;
+  if (N >= 1000000000000) ans += N - 999999999999;
+  if (N >= 1000000000000000) ans += N - 999999999999999;
+  cout << ans << endl;
+  return 0;
+}
