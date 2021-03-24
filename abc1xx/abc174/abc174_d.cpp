2fea4b818b0ad186099dc97c61e6a5dcdbc7682f
irori <cats.cafemocha+irori@gmail.com>
2021-03-25 00:15:47 +0900

    20210325_01

---
+++ abc1xx/abc174/abc174_d.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  int a = 0, b = 0;
+  rep(i, N) if (S[i] == 'R') a++;
+  int ans = max(a, b);
+  rep(i, N) {
+    if (S[i] == 'R')
+      a--;
+    else
+      b++;
+    ans = min(ans, max(a, b));
+  }
+  cout << ans << endl;
+  return 0;
+}
