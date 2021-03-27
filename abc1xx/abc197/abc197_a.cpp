8a0e75ce0e78dd1317041b49e1f25c7467f2e681
irori <cats.cafemocha+irori@gmail.com>
2021-03-28 00:13:31 +0900

    20210328_01

---
+++ abc1xx/abc197/abc197_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  cout << S[1] << S[2] << S[0] << endl;
+  return 0;
+}
