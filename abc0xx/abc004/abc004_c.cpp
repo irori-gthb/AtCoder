c306dc745f5023f5cc056d8e8a92d7680c105953
irori <cats.cafemocha+irori@gmail.com>
2021-03-29 00:04:00 +0900

    20210329_01

---
+++ abc0xx/abc004/abc004_c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> v = {1, 2, 3, 4, 5, 6};
+  N %= 30;
+  rep(i, N) {
+    int j = i % 5;
+    swap(v[j], v[j + 1]);
+  }
+  for (int x : v) cout << x;
+  cout << endl;
+  return 0;
+}
