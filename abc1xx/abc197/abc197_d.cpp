b0f3f7b59ddccecca64fbcfd753adb7fe09e9761
irori <cats.cafemocha+irori@gmail.com>
2021-03-30 21:06:51 +0900

    20210330_01

---
+++ abc1xx/abc197/abc197_d.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n;
+  double x, y;
+  complex<double> a, b;
+
+  cin >> n;
+  cin >> x >> y;
+  a = {x, y};
+  cin >> x >> y;
+  b = {x, y};
+  auto m = (a + b) / 2.0;
+  auto res = m + (a - m) * polar(1.0, M_PI * 2.0 / n);
+  cout << setprecision(9) << res.real() << " " << res.imag() << endl;
+  return 0;
+}
