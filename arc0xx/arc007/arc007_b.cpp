bf3e7311d60dee37a0eaa2cbe4f59ebe023118a6
irori <cats.cafemocha+irori@gmail.com>
2021-04-04 01:22:26 +0900

    20210404_01

---
+++ arc0xx/arc007/arc007_b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  int a[N + 1];
+  rep(i, N + 1) a[i] = i;
+
+  rep(i, M) {
+    int d;
+    cin >> d;
+    rep(j, N + 1) {
+      if (a[j] == d) {
+        swap(a[j], a[0]);
+        break;
+      }
+    }
+  }
+  rep(i, N) cout << a[i + 1] << endl;
+  return 0;
+}
