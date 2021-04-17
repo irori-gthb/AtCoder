d4ddd1d30197dab685c743f7064d127b04a6730d
irori <cats.cafemocha+irori@gmail.com>
2021-04-18 00:12:52 +0900

    20210418_01

---
+++ others/jsc2021/jsc2021_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int X, Y, Z;
+  cin >> X >> Y >> Z;
+  cout << (Y * Z - 1) / X << endl;
+  return 0;
+}
