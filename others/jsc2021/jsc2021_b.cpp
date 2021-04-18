555210f555e102a030a085f48fb99cd85b3a5bdc
irori <cats.cafemocha+irori@gmail.com>
2021-04-19 00:07:37 +0900

    20210419_01

---
+++ others/jsc2021/jsc2021_b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  vector<int> arr(1001);
+  int j;
+  rep(i, N + M) {
+    cin >> j;
+    arr[j]++;
+  }
+
+  rep(i, 1005) if (arr[i] == 1) cout << i << ' ';
+  cout << endl;
+  return 0;
+}
