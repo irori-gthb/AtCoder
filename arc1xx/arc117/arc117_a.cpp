87cfd620cc4f733b6fe52fb1532eaa6ee54245f3
irori <cats.cafemocha+irori@gmail.com>
2021-04-21 00:20:22 +0900

    20210421_01

---
+++ arc1xx/arc117/arc117_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int calc = 0;
+  for (int i = min(A, B); i <= max(A, B); i++) calc += i;
+
+  if (A > B) {
+    rep(i, A) cout << i + 1 << " ";
+    rep(i, B - 1) cout << -(i + 1) << " ";
+    cout << -calc;
+  } else if (A < B) {
+    rep(i, A - 1) cout << i + 1 << " ";
+    rep(i, B) cout << -(i + 1) << " ";
+    cout << calc;
+  } else {
+    rep(i, A) cout << i + 1 << " ";
+    rep(i, B) cout << -(i + 1) << " ";
+  }
+  return 0;
+}
