c1ca6f91d2cc7a649430559c2c005c477c056ecd
irori <cats.cafemocha+irori@gmail.com>
2021-05-10 00:07:33 +0900

    20210510_01

---
+++ abc2xx/abc200/abc200_b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+
+  ll ans = N;
+  rep(i, K) {
+    if (ans % 200 == 0)
+      ans /= 200;
+    else
+      ans = ans * 1000 + 200;
+  }
+  cout << ans << endl;
+  return 0;
+}
