ac7842c8777062043d75dbfffd3e09439f5a6411
irori <cats.cafemocha+irori@gmail.com>
2021-05-14 00:04:39 +0900

    20210514_01

---
+++ abc1xx/abc171/abc171_e.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<ll> a(N);
+  rep(i, N) cin >> a[i];
+
+  ll calc = 0;
+  rep(i, N) calc ^= a[i];
+
+  rep(i, N) cout << (a[i] ^ calc) << ' ';
+  cout << endl;
+  return 0;
+}
