30a9d641b2547c74719e9392f2d673440835c28c
irori <cats.cafemocha+irori@gmail.com>
2021-05-21 08:58:42 +0900

    20210521_01

---
+++ arc0xx/arc030/arc030_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  if (2 * k <= n)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+  return 0;
+}
