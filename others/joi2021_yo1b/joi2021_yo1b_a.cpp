e5f95564afb4a86474114546efb3e70fc939427a
irori <cats.cafemocha+irori@gmail.com>
2021-06-10 00:10:11 +0900

    20210610_01

---
+++ others/joi2021_yo1b/joi2021_yo1b_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  if (A <= C && C < B)
+    cout << 1 << endl;
+  else
+    cout << 0 << endl;
+  return 0;
+}
