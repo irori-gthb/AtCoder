f46c43fc1b028247604f9ba842bca34a87417106
irori <cats.cafemocha+irori@gmail.com>
2021-06-28 00:08:21 +0900

    20210628_01

---
+++ abc2xx/abc207/abc207_b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double A, B, C, D;
+  cin >> A >> B >> C >> D;
+  if (D * C - B < 1)
+    cout << -1 << endl;
+  else
+    cout << (ceil((A) / (C * D - B))) << endl;
+  return 0;
+}
