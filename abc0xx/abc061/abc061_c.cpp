8cc1ca317babf63e1844be48fe50c778a990ddb4
irori <cats.cafemocha+irori@gmail.com>
2021-07-10 00:27:37 +0900

    20210710_01

---
+++ abc0xx/abc061/abc061_c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  ll K;
+  cin >> N >> K;
+
+  ll a, b;
+  map<ll, ll> mp;
+  rep(i, N) cin >> a >> b, mp[a] += b;
+
+  ll calc = 0;
+  for (auto x : mp) {
+    calc += x.second;
+    if (K <= calc) {
+      cout << x.first << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
