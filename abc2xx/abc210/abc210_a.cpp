613ab2c57cf132a47f6f94f7c7389531912cdc32
irori <cats.cafemocha+irori@gmail.com>
2021-07-18 00:41:12 +0900

    20210718_01

---
+++ abc2xx/abc210/abc210_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, A, X, Y;
+  cin >> N >> A >> X >> Y;
+
+  if (N <= A)
+    cout << N * X << endl;
+  else
+    cout << X * A + (N - A) * Y << endl;
+  return 0;
+}
