657a2266b9ae2cb865aa776d1371491242fa0d3c
irori <cats.cafemocha+irori@gmail.com>
2021-07-26 00:14:12 +0900

    20210726_01

---
+++ abc2xx/abc211/abc211_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string s;
+  set<string> st;
+  rep(i, 4) cin >> s, st.insert(s);
+
+  if (st.size() == 4)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
