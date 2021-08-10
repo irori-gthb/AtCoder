477668ae19764141f8256afb90a889a462c202d1
irori <cats.cafemocha+irori@gmail.com>
2021-08-10 09:16:17 +0900

    20210810_01

---
+++ abc2xx/abc213/abc213_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<pair<int, int>> A(N);
+  rep(i, N) cin >> A[i].first, A[i].second = i + 1;
+
+  sort(A.begin(), A.end());
+  cout << A[N - 2].second << endl;
+  return 0;
+}
