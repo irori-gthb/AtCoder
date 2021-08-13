50cb87f17a281e468371d2e4f3fb4a753aef2dbc
irori <cats.cafemocha+irori@gmail.com>
2021-08-14 00:03:20 +0900

    20210814_01

---
+++ arc0xx/arc001/arc001_b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int p[10] = {0, 1, 2, 3, 2, 1, 2, 3, 3, 2};
+  int calc = max(A, B) - min(A, B);
+  cout << calc / 10 + p[calc % 10] << endl;
+  return 0;
+}
