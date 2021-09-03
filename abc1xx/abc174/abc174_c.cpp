ab37f958f3616175c1edc342892330e93fed534b
irori <cats.cafemocha+irori@gmail.com>
2021-09-04 01:10:57 +0900

    20210904_01

---
+++ abc1xx/abc174/abc174_c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int K;
+  cin >> K;
+
+  int ans = 1;
+  ll calc = 7;
+
+  while (calc % K && ans <= K) {
+    calc = (calc * 10 + 7) % K;
+    ans++;
+  }
+  if (ans == K + 1)
+    cout << -1 << endl;
+  else
+    cout << ans << endl;
+  return 0;
+}
