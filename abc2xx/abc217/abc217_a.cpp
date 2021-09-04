8f03fed365f80e8ef3359a1aa6601684affcadbb
irori <cats.cafemocha+irori@gmail.com>
2021-09-05 00:04:59 +0900

    20210905_01

---
+++ abc2xx/abc217/abc217_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S, T;
+  cin >> S >> T;
+
+  if (S < T)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
