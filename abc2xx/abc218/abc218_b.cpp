b0bedc5b4b85c21a708289b9254871f96392c746
irori <cats.cafemocha+irori@gmail.com>
2021-09-12 00:18:06 +0900

    20210912_02

---
+++ abc2xx/abc218/abc218_b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string ans;
+  int p;
+
+  string str = "abcdefghijklmnopqrstuvwxyz";
+  rep(i, 26) {
+    cin >> p;
+    ans += str[p - 1];
+  }
+  cout << ans << endl;
+  return 0;
+}
