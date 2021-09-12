3e6940aead68dc4109c709dda42b33703c9844a8
irori <cats.cafemocha+irori@gmail.com>
2021-09-13 00:23:44 +0900

    20210913_01

---
+++ abc0xx/abc005/abc005_c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int T, N, M;
+  cin >> T >> N;
+
+  vector<int> A(N);
+  rep(i, N) cin >> A[i];
+
+  cin >> M;
+  vector<int> B(M);
+  rep(i, M) cin >> B[i];
+
+  int t = 0, c = 0;
+  while (t < N && c < M) {
+    if (A[t] <= B[c] && B[c] <= A[t] + T) c++;
+    t++;
+  }
+
+  if (t <= N && c == M)
+    cout << "yes" << endl;
+  else
+    cout << "no" << endl;
+  return 0;
+}
