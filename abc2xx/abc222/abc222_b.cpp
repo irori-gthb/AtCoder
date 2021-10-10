2847d4e02fe7c4655489e7721ac77fe0f3e8b274
irori <cats.cafemocha+irori@gmail.com>
2021-10-11 00:13:23 +0900

    20211011_01

---
+++ abc2xx/abc222/abc222_b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N, P;
+  cin >> N >> P;
+
+  int ans = 0;
+  int a;
+  rep(i, N) {
+    cin >> a;
+    if (a < P) ans++;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
