ca7d82f321c4d1d2fa528215ae4ac601365717a4
irori <cats.cafemocha+irori@gmail.com>
2021-10-24 09:14:57 +0900

    20211024_01

---
+++ abc2xx/abc224/abc224_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S[S.size() - 1] == 'r')
+    cout << "er" << endl;
+  else
+    cout << "ist" << endl;
+  return 0;
+}
