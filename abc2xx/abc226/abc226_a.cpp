8e4cc2c4f4ac3603c1df7a35aeefd5b31204cee7
irori <cats.cafemocha+irori@gmail.com>
2021-11-08 00:04:46 +0900

    20211108_01

---
+++ abc2xx/abc226/abc226_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  double X;
+  cin >> X;
+
+  cout << (int)(X + 0.5) << endl;
+  return 0;
+}
