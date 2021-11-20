c0d80fb1b8c5709b07df5db815551b4ae17c67c4
irori <cats.cafemocha+irori@gmail.com>
2021-11-21 00:19:57 +0900

    20211121_01

---
+++ abc2xx/abc228/abc228_a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int S, T, X;
+  cin >> S >> T >> X;
+
+  if (S < T) {
+    if (S <= X && X < T)
+      cout << "Yes" << endl;
+    else
+      cout << "No" << endl;
+  } else {
+    if (X < T || S <= X)
+      cout << "Yes" << endl;
+    else
+      cout << "No" << endl;
+  }
+  return 0;
+}
