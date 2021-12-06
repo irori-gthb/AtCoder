0ba231aa758878b52c85f1a77a034837f4d53b30
irori <cats.cafemocha+irori@gmail.com>
2021-12-06 12:07:28 +0900

    20211206_01

---
+++ arc1xx/arc131/arc131_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll A, B;
+  cin >> A >> B;
+  cout << 1000000000 * A + 5 * B << endl;
+  return 0;
+}
